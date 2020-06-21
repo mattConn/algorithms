dc8e916509f0adf1de143a22dda185628ac7b6d4
Matthew Connelly <matthew.b.connelly@gmail.com>
2020-06-21 04:28:05 -0400

    executing external python script with output as args

--- sum-subsets-graph-theory/array-based-sum-tree.go
+++ sum-subsets-graph-theory/array-based-sum-tree.go
@@ -2,6 +2,8 @@ package main
 
 import (
 	"fmt"
+	"os/exec"
+	"strconv"
 )
 
 // make array-based binary tree for sums of subsets of input
@@ -15,8 +17,8 @@ func fillSumTree (input, output []int, n, i int) {
 	output[2*n+2] = output[n] // right node
 
 	// i+1 for each call to match depth
-	makeSumTree(input,output,2*n+1,i+1) // descend left 
-	makeSumTree(input,output,2*n+2,i+1) // descend right
+	fillSumTree(input,output,2*n+1,i+1) // descend left 
+	fillSumTree(input,output,2*n+2,i+1) // descend right
 }
 
 func main(){
@@ -26,5 +28,14 @@ func main(){
 
 	fillSumTree(nums,output,0,0)
 
-	fmt.Println(output)
+	outputString := []string{}
+
+	for _,v := range output {
+		outputString = append(outputString,strconv.Itoa(v))
+	}
+
+	err := exec.Command("./graph-plotter.py", outputString...).Run()
+	if err != nil {
+		fmt.Println(err)
+	}
 }
