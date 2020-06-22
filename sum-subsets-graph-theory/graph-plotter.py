abb0160f2c030bb9d9f302e4914318eb5099e405
Matthew Connelly <matthew.b.connelly@gmail.com>
2020-06-21 20:44:22 -0400

    displaying depth of node in plot

--- sum-subsets-graph-theory/graph-plotter.py
+++ sum-subsets-graph-theory/graph-plotter.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python
 
 import sys
+import math
 import networkx as nx
 import matplotlib.pyplot as plt
 import pygraphviz
@@ -9,7 +10,8 @@ from networkx.drawing.nx_agraph import graphviz_layout
 # graph drawing options
 def draw(graph):
     pos=graphviz_layout(graph, prog='dot')
-    nx.draw_networkx(graph,pos,font_color='black',font_size=15,node_size=700)
+    nx.draw_networkx(graph,pos,font_color='black',font_size=12,node_size=700,)
+    plt.title("depth:node:sum")
 
 sys.argv[1:] = [int(i) for i in sys.argv[1:]]
 
@@ -22,17 +24,18 @@ g = nx.DiGraph();
 #for i in sys.argv[1:]:
 #    g.add_node(i)
 
-for depth in range(len(sys.argv[1:])):
-    leftIndex = 2*depth+1
-    rightIndex = 2*depth+2
+for dataIndex in range(len(sys.argv[1:])):
+    leftIndex = 2*dataIndex+1
+    rightIndex = 2*dataIndex+2
 
     if leftIndex >= len(sys.argv[1:]): break 
 
+    def getDepth(index): return int(math.log(index+1,2))
+
     # for connecting current node to another
-    # labelling each node as "depth:data[depth]"
-    def joinCurNodeTo(nodeIndex):
-        g.add_edge(f'{depth}:{sys.argv[1:][depth]}', f'{nodeIndex}:{sys.argv[1:][nodeIndex]}')
-        print(f'JOINING: {depth}:{sys.argv[1:][depth]} TO {nodeIndex}:{sys.argv[1:][nodeIndex]}')
+    # labelling each node as "dataIndex:data[dataIndex]"
+    def joinCurNodeTo(otherIndex):
+        g.add_edge(f'{getDepth(dataIndex)}:{dataIndex}:{sys.argv[1:][dataIndex]}', f'{getDepth(otherIndex)}:{otherIndex}:{sys.argv[1:][otherIndex]}')
 
     joinCurNodeTo(leftIndex)
     joinCurNodeTo(rightIndex)
